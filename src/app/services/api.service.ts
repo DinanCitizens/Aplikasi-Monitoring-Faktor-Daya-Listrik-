752f2de428db2aa5d625f3e7059388e0d3cb1215
farisbagas55 <farisbagas55@gmail.com>
2020-12-01 20:23:45 +0700

    Menambah url dan param untuk dipanggil di rekapitulasi

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,11 +3,7 @@ import { Observable, of, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { catchError, tap, map } from 'rxjs/operators';
 
-const httpOptions = {
-	headers: new HttpHeaders({'Content-Type': 'application/json'})
-};
-
-const apiUrl = "https://api.thingspeak.com/channels/1092085/feeds.json?api_key=YJQJLM4J0A3IP1QU&results=10";
+const apiUrl = "https://api.thingspeak.com/channels/1092085/feeds.json?api_key=YJQJLM4J0A3IP1QU&timezone=Asia/Jakarta";
 
 @Injectable({
   providedIn: 'root'
@@ -16,24 +12,16 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-  private handleError(error: HttpErrorResponse){
-  	if (error.error instanceof ErrorEvent){
-  		console.error('An error occured', error.error.message);
-  	} else{
-  		console.error(
-  			'Backend returned code ${error.status}, ' +
-  			'body was: ${error.error}');
-  	}
-  	return throwError('Something bad happened');
+  get(start, end): Promise<any>{
+  	return this.http.get(apiUrl, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      params: {
+        start: start,
+        end: end
+      }
+    }).toPromise();
   }
 
-  private extractData(res: Response){
-  	let body = res;
-  	return body || {};
-  }
-  getDataField(): Observable<any>{
-  	return this.http.get(apiUrl, httpOptions).pipe(
-  		map(this.extractData),
-  		catchError(this.handleError));
-  }
 }
